efeec6d0039d5c0e360ddfd67dbfc1b498a8c734
Niels Grewe <grewe@ocean-insights.com>
2020-09-30 18:31:04 +0200

    fix: Get into a working state

--- faucet.config.js
+++ faucet.config.js
@@ -1,8 +1,24 @@
+const kind = process.env.NODE_ENV == "production" ? "production.min" : "development";
+
 module.exports = {
 	js: [{
 		source: "./index.jsx",
 		target: "./dist/index.js",
 		jsx: true,
-		esnext: {exclude: ["*"]}
-	}]
+		esnext: {exclude: ["*"]},
+		externals: {
+			react: "React",
+			"react-dom": "ReactDOM",
+		}
+	}],
+	static: [
+        {
+            source: `react/umd/react.${kind}.js`,
+            target: "./dist/react.js",
+        },
+        {
+            source: `react-dom/umd/react-dom.${kind}.js`,
+            target: "./dist/react-dom.js",
+        }
+    ]
 };
